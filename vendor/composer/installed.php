d654cd8f5c8f34b9e9d276c9cbdc9fbae5b9b2ff
RybarzLukasz <kkfk.kkfk2@o2.pl>
2022-02-03 15:31:37 +0100

    IGC

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '21e2b20785b5107074fec9db54e070727cc1851e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '21e2b20785b5107074fec9db54e070727cc1851e',
+    ),
+    'alrik11es/cowsayphp' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '080dc10c8f5420fef35163569293601f17124eeb',
+    ),
+  ),
+);
